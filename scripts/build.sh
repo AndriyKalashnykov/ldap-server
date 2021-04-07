f579158327be8ee3b1e18d7b32345089debda5b6
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2021-04-07 18:12:27 -0400

    added build/push scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,11 +8,7 @@ LAUNCH_DIR=$(pwd); SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"; c
 
 cd $ACTIVEMQ_VER
 
-docker rmi -f ${DOCKER_LOGIN}/$ACTIVEMQ_IMAGE_NAME:$ACTIVEMQ_VER
-DOCKER_BUILDKIT=1 docker build -f Dockerfile -t ${DOCKER_LOGIN}/$ACTIVEMQ_IMAGE_NAME:$ACTIVEMQ_VER .
-
-cd ../apacheds-ad
-
+#docker rmi -f ${DOCKER_LOGIN}/$APACHEDS_IMAGE_NAME:$APACHEDS_VER
 DOCKER_BUILDKIT=1 docker build -f Dockerfile -t ${DOCKER_LOGIN}/$APACHEDS_IMAGE_NAME:$APACHEDS_VER .
 
 cd $LAUNCH_DIR
