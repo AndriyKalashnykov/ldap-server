aaf0af612dbe7c214e4b6367c0765d11d792eb17
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2021-06-29 23:29:13 -0400

    updated log4j libs

---
+++ scripts/local-run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# set -x
+
+LAUNCH_DIR=$(pwd); SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"; cd $SCRIPT_DIR; cd ..; SCRIPT_PARENT_DIR=$(pwd);
+
+. $SCRIPT_DIR/set-env.sh
+
+cd $SCRIPT_DIR/..
+
+mvn clean install
+java -jar ./target/ldap-server.jar /ldap/ldif/
+
+cd $LAUNCH_DIR
